2117e45dfbcd08f6d900580b1516f3357b99a488
İrfan Sütlü <peroxcid@haotmail.com>
2022-11-16 16:23:39 +0300

    recipe-app

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="about">
+      <img src="https://irfansutlu.github.io/mywebsite/img/irfanpp-removebg-preview.png" />
+      <div className="about-info">
+        <h3>
+          Welcome, I am Irfan. Nice to meet you. I am a web developer. I am a
+          junior programmer with good knowledge of front-end techniques. I love
+          structure and order and I also stand for quality. I love spending time
+          on fixing little details and optimizing web apps. Also I like working
+          in a team, I'll learn faster and much more.
+        </h3><br />
+        <a href="https://github.com/irfansutlu">GitHub</a><br /><br /><br />
+        <a href="https://irfansutlu.github.io/mywebsite/">Personal Website</a><br />
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function Details() {
+  const { state: details } = useLocation();
+  const navigate = useNavigate();
+  console.log(details);
   return (
-    <div>Details</div>
-  )
+    <div className="detail">
+      <h1>{details.label}</h1>
+      <div className="info-div">
+        <div className="values">
+          <h3>Nutriens</h3>
+          <hr />
+          <ul>
+            <li>Calcium:{details.digest[5].total.toFixed(2)}mg</li>
+            <li>Carbs:{details.digest[1].total.toFixed(2)}g</li>
+            <li>Calcium:{details.digest[5].total.toFixed(2)}mg</li>
+            <li>Cholestrol:{details.digest[3].total.toFixed(2)}mg</li>
+            <li>Energy:{details.calories.toFixed(2)}kcal</li>
+            <li>Fat:{details.digest[0].total.toFixed(2)}</li>
+            <li>Protein:{details.digest[2].total.toFixed(2)}</li>
+          </ul>
+        </div>
+        <img src={details.image} />
+        <div className="materials">
+          {details.ingredientLines.map((list, idx) => (
+            <ul>
+              <li key={idx}>{list}</li>
+            </ul>
+          ))}
+        </div>
+      </div>
+      <button onClick={() => navigate(-1)}>Go Back</button>
+    </div>
+  );
 }
 
-export default Details
+export default Details;
